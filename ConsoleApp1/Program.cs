7433ffffea0ef0a83daeedd277eb1c71d86d18f8
VladkaLipskaja <33036348+VladkaLipskaja@users.noreply.github.com>
2019-02-09 18:46:51 +0200

    console app

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace ConsoleApp1
 {
-    class Program
+    public static class Program
     {
+        public class A
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public static IOrderedQueryable<TSource> OrderWithSeveralRulesBy<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> mainRule, params Expression<Func<TSource, object>>[] rules)
+        {
+            var query = source.OrderBy(mainRule);
+
+            foreach (var rule in rules)
+            {
+                query = query.ThenBy(rule);
+            }
+
+            return query;
+        }
+
         static void Main(string[] args)
         {
+            List<A> a = new List<A>
+            {
+                new A { Id = 1, Name = "Name1" },
+                new A { Id = 2, Name = "Name2"}
+            };
+
+            a.AsQueryable().OrderWithSeveralRulesBy(i => i.Id, i => i.Id, i => i.Name);
+            Console.ReadKey();
         }
     }
 }
